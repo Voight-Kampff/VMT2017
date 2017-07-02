67f5ea9835f8a87b87a405c54adb14619c7e4ad9
Voight-Kampff <valfells@gmail.com>
2017-07-02 23:57:46 +0100

    changed env

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,8 +2,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
-      :aws_access_key_id      => Rails.application.secrets.amazon_access_key_id,
-      :aws_secret_access_key  => Rails.application.secrets.amazon_secret_access_key,
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['SECRET_KEY_BASE']
       :region                 => 'eu-west-1' 
   }
   config.fog_directory  = 'variations'
