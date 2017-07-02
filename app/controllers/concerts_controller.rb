48c8afefb1339c318a671d5788b8075e3827d83f
Voight-Kampff <valfells@gmail.com>
2017-07-03 00:26:39 +0100

    if condition

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -8,7 +8,7 @@ class ConcertsController < ApplicationController
   def create
     @concert=Concert.create(concert_params)
     if @concert.save
-      if @concert.seating.nil?
+      if @concert.unnumbered.nil?
         rows=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S"]
         0..475.times do |i|
 
