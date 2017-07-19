f348b2f15b92ba6688c9f7da19bd50e6c0006bfe
Voight-Kampff <valfells@gmail.com>
2017-07-19 13:53:00 +0200

    changed invitation email

--- app/mailers/ticket_mailer.rb
+++ app/mailers/ticket_mailer.rb
@@ -1,27 +1,11 @@
 class TicketMailer < ApplicationMailer
 
-	def invitation(order)
+	def invitation(invitation)
 
-		@order = order
-		@reservations=@order.reservations
+		@order = invitation.order
+		@invitation=invitation
 
-		qr= RQRCode::QRCode.new(@order.id.to_s+"-"+@order.code.to_s)
-		@qr_png = qrcode.as_png(
-			resize_gte_to: false,
-	        resize_exactly_to: false,
-	        fill: 'white',
-	        color: 'black',
-	        size: 260,
-	        file: nil # path to write
-          )
-
-		open(@reservations.last.pdf_url) do |url_file|
-  			tmp_file.write(url_file.read)
-		end
-
-		attachments[@reservations.last.pdf_name.to_s]= tmp_file.read
-
-    	mail(:to => @order.email, :from => "Billetterie@musicales-tannay.ch", :bcc => "webmaster@musicales-tannay.ch", :subject => "Vos billets pour les Variations Musicales de Tannay")
+    	mail(:to => @invitation.email, :from => "Billetterie@musicales-tannay.ch", :bcc => "webmaster@musicales-tannay.ch", :subject => "Vos invitations pour les Variations Musicales de Tannay")
 	end
 
 	def ticket(order)
