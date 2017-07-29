c6fc1811b5b05b27dd62b5eff33e2d0d0b7db295
Voight-Kampff <valfells@gmail.com>
2017-07-29 18:11:52 +0200

    attempting again

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+if Rails.env == 'production'
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
