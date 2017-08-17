e5a2e960e399ce9a9dd4d054e03cc4529e6a3422
Voight-Kampff <valfells@gmail.com>
2017-08-17 09:57:21 +0200

    changed seat_code generation

--- app/models/seat_code.rb
+++ app/models/seat_code.rb
@@ -4,11 +4,13 @@ class SeatCode < ApplicationRecord
 	before_save :assign_params
 
 	def assign_params
-
-		self.row = self.seat.row
-		self.column = self.seat.column
-		self.concert_name = self.seat.concert.name
-		self.concert_date = (I18n.localize self.seat.concert.date, format: :concert).capitalize
+		
+		unless self.seat.nil?
+			self.row = self.seat.row
+			self.column = self.seat.column
+			self.concert_name = self.seat.concert.name
+			self.concert_date = (I18n.localize self.seat.concert.date, format: :concert).capitalize
+		end
 
 		if self.code.nil?
 			self.code = SecureRandom.urlsafe_base64(20)
