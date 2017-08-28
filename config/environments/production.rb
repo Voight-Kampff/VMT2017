804d8afbd983e179bdb078c7e7fd0f44529f4f35
Voight-Kampff <valfells@gmail.com>
2017-08-28 21:28:21 +0000

    fixed cable issue... so obvious in retrospect...
    It was config.action_cable-url not config:web:sockt_server_url…

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
   #Seting for invitaiton mailer
   config.action_mailer.default_url_options = { host: 'vmt-tickets2.herokuapp.com'}
 
-  config.web_socket_server_url = "wss://vmt-tickets2.herokuapp.com/cable" 
+  config.action_cable.url = "wss://vmt-tickets2.herokuapp.com/cable" 
   config.action_cable.allowed_request_origins = ['https://vmt-tickets2.herokuapp.com', 'http://vmt-tickets2.herokuapp.com']
   config.action_cable.disable_request_forgery_protection = true
 
