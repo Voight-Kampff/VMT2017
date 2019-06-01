aea425807b29b62a1f5d2770e199b61faa220353
Voight-Kampff <valfells@gmail.com>
2019-06-01 16:48:09 +0200

    fixed issue

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,14 +1,12 @@
 class AdminController < ApplicationController
 
 	#includes the secured concern (module) for auth0
-	Rails.env.production?
-		include Secured
-	end
+	include Secured
 
 
 	#acts as a root page of sorts for the admin section. Currently a test page
 	def dashboard
-	
+		
 	end
 
 end
