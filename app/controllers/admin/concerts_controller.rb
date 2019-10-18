b394da7a4a7290f539b9b89627a045e2e271f9cd
Voight-Kampff <valfells@gmail.com>
2019-10-18 10:50:02 +0200

    Master 2.2.b
    Limited types to concert specific types in
    Admin::Concerts::Controller::Show
    Fixed issue with enumerator in Admin::Concerts::Show

--- app/controllers/admin/concerts_controller.rb
+++ app/controllers/admin/concerts_controller.rb
@@ -7,11 +7,12 @@ class Admin::ConcertsController < ApplicationController
 
 	def show
 		@concert=Concert.find_by_id(params[:id])
-		@types=ReservationType.all
+		@types=@concert.reservation_types
 		@keys= @types.map{|i| i.id}
 		@stats=Array.new
 		@keys.each do |i|
-			@stats[i]=@concert.reservations.where(reservation_type_id: @keys[i]).count
+				@stats.push((@concert.reservations.where(reservation_type_id: @keys[i-1])).count)
+
 		end
 
 	end
