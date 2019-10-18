9bd96a2b0d27853a6acee9a70db8ece66a84c2d0
Voight-Kampff <valfells@gmail.com>
2019-10-18 09:52:57 +0200

    Master 2.2.a
    Added stats on admin/concert page
    -Query method to get stats in Admin::Concerts controller
    -Simple display added to Admin::Concerts::Show

--- app/controllers/admin/concerts_controller.rb
+++ app/controllers/admin/concerts_controller.rb
@@ -5,4 +5,15 @@ class Admin::ConcertsController < ApplicationController
 	def index
 	end
 
+	def show
+		@concert=Concert.find_by_id(params[:id])
+		@types=ReservationType.all
+		@keys= @types.map{|i| i.id}
+		@stats=Array.new
+		@keys.each do |i|
+			@stats[i]=@concert.reservations.where(reservation_type_id: @keys[i]).count
+		end
+
+	end
+
 end
